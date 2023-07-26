c7f151e2d7d415cdc17f3cb0858b7fc74ba187a7
popipopipoi <somt1128@gmail.com>
2023-07-26 14:03:30 +0900

    1

---
+++ src/com/ohgiraffers/section01/thread/Application.java
@@ -0,0 +1,63 @@
+package com.ohgiraffers.section01.thread;
+
+public class Application {
+
+    public static void main(String[] args) {
+        /* Thread를 생성하는 방법
+        * 1. Tread 클래스 상속
+        * 2. Runnable 인터페이스 구현
+        * */
+
+        Car car = new Car();
+        Tank tank = new Tank();
+        Plane plane = new Plane();
+
+        /* Thread 타입의 인스턴스로 변환 */
+        Thread t1 = car;
+        Thread t2 = tank;
+        /* Runnable을 상속 받은 클래스는 Thread 클래스의 생성자에 인자로 인스턴스를 전달해서 Thread 인스턴스를 생성한다. */
+        Thread t3 = new Thread(plane);
+
+        /* 다형성을 적용해서 Tread 타입으로 바로 인스턴스 생성 */
+        Thread t4 = new Car();
+        Thread t5 = new Tank();
+        Thread t6 = new Thread(new Plane());
+
+        /* 기본적으로 가지고 있는 우선순위 확인 */
+        System.out.println("t4 우선 순위 : " + t4.getPriority());
+        System.out.println("t5 우선 순위 : " + t5.getPriority());
+        System.out.println("t6 우선 순위 : " + t6.getPriority());
+
+        /* 우선 순위 변경 */
+        t4.setPriority(Thread.MAX_PRIORITY);
+        t6.setPriority(Thread.MIN_PRIORITY);
+
+        /* 변경된 우선순위 확인 */
+        System.out.println("t4 우선 순위 : " + t4.getPriority());
+        System.out.println("t5 우선 순위 : " + t5.getPriority());
+        System.out.println("t6 우선 순위 : " + t6.getPriority());
+
+        /* run() 메소드를 호출하면 별도의 스레드로 동작하지 않고 메소드를 호출하는 방식으로 동작한다. */
+//        t4.run();
+//        t5.run();
+//        t6.run();
+
+        /* start() 메소드를 호출하면 각각의 스레드로 동작한다. (별도의 호출 스택을 생성) */
+        t4.start();
+        t5.start();
+        t6.start();
+
+        /* 지정한 스레드가 종료 될 때까지 현재 메인 스레드의 종료를 대기 시킨다. */
+        try {
+            t4.join();
+            t5.join();
+            t6.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("======== main end");
+
+
+    }
+}
---
+++ src/com/ohgiraffers/section02/daemonthread/CountDown.java
@@ -0,0 +1,16 @@
+package com.ohgiraffers.section02.daemonthread;
+
+public class CountDown extends Thread {
+
+    @Override
+    public void run() {
+        for (int i = 10; i >= 0 ; i--) {
+            System.out.println(i);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
